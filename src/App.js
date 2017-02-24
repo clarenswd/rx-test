48e25cd9f64d1dcee31c1ba0f2fe4f5683da31c5
clarens <clarens007@gmail.com>
2017-02-24 13:53:58 +1000

    CORS issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+var md5 = require('md5');
+
 
 class App extends Component {
   constructor(props){
@@ -8,8 +10,14 @@ class App extends Component {
     this.authenticate = this.authenticate.bind(this);
   }
   authenticate(){
+    let ts = '1';
+    let pk = 'd16f65a18b67e96a5e7bd55de1c675c4';
+    let pub_key = 'b128578135ff747b60902c97bb100a9c43185337';
 
-    fetch('http://gateway.marvel.com:80/v1/public/characters?name=Spider-Man&apikey=d16f65a18b67e96a5e7bd55de1c675c4').then(function(response) {
+    let hashed_data = md5(ts + pk + pub_key);
+    let url_end_point = "http://gateway.marvel.com/v1/comics?ts="+ts+"&apikey="+ pk + "&hash="+hashed_data;
+
+    fetch(url_end_point).then(function(response) {
         console.log( response);
     })
   }
