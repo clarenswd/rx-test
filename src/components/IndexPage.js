f54e7c40e9376aa5f1d8ba4ea7fabcd86f0b4426
clarenswd <clarens007@gmail.com>
2017-02-26 01:57:19 +1000

    pulling some json inside React

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -3,13 +3,27 @@ import React from 'react';
  
 
 export default class IndexPage extends React.Component {
-  render() {
-    return (
-      <div className="home">
-        <div className="">
-            Index page
-        </div>
-      </div>
-    );
+    constructor(props){
+        super(props);
+        this.state={data:""};
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:8080/marvel/comics")
+            .then( (response) => {
+                return response.json() })   
+                    .then( (json) => {
+                        this.setState({data: json});
+                    });
+    };
+    render() {
+        return (
+          <div className="home">
+            <div className="">
+                Index page
+                {this.state.data}
+            </div>
+          </div>
+        );
   }
 }
