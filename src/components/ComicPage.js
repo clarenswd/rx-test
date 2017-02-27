853013b0221f71e90d75c21d9ebca8c7a75cc6b2
clarens <clarens007@gmail.com>
2017-02-27 15:33:18 +1000

    desperate times, desperate measures, using another ref var to grab the state

--- src/components/ComicPage.js
+++ src/components/ComicPage.js
@@ -8,39 +8,52 @@ import Comic from './Comic';
 export default class ComicPage extends React.Component {
   constructor(props){
     super(props);
-    this.state = {comic:{}};
+    this.state = {
+      comic:{}, 
+    };
     this.requestRelatedData = this.requestRelatedData.bind(this);
+
    }
   requestRelatedData(endpoint){
     //this.props.params.id
+      let react_this = this;
       fetch("http://localhost:5000/marvel/comic/" + endpoint)
             .then( (response) => { return response.json() })   
             .then( (json) => {
                 let jsonObj = JSON.parse(json);
-                console.log(jsonObj);
+ 
 
                 //CHECK THIS 
-                this.setState({comic:jsonObj.data.results[0] });
+                this.setState({
+                  comic:jsonObj.data.results[0] 
+                });
+                
+            })
+            .then(function(){
+               react_this.setState({thumbnail: react_this.state.comic.thumbnail.path + '.' + react_this.state.comic.thumbnail.extension });
             });
   } 
   componentDidMount() {
-    console.log("cdm");
     this.requestRelatedData(this.props.params.id);
-     
+   
   }
 
 
   render() {
     const id = this.props.params.id;
+    let image ;
+    if(this.state.thumbnail !== 'undefined'){
+      image = <img src= {this.state.thumbnail}/>
+    } 
     
     return (
       <div className="comic-full">
+        <div className="imgWrapper">
+           {image}
+        </div>
         <h1 className="title">{this.state.comic.title}</h1>
         <p>{this.state.comic.description}</p>
       </div>
-
-
-
     );
   }
 }
