ba4e18709febc674a9a1c39483bf9767a222067a
clarenswd <clarens007@gmail.com>
2017-02-27 21:56:51 +1000

    styles for character list

--- src/components/CharacterListItem.js
+++ src/components/CharacterListItem.js
@@ -4,11 +4,17 @@ import { Link } from 'react-router';
 
 export default class CharacterListItem extends React.Component {
     render() {
+        let thumbnail_url =this.props.thumbnail.path + '.' + this.props.thumbnail.extension; 
         return (
             <li>
               <div className="character" >
+                  <div className="character_pic">
+                      <img src={thumbnail_url}/>
+                  </div>
+                  <div className="character_info">
                        <h1 className="character_name">{this.props.name}</h1>
                        <p>{(this.props.description)? this.props.description.substr(0,200)+"...":"No description for this character"}</p>
+                  </div>
               </div>
             </li>
          
